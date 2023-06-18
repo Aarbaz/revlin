b6fcccefc0f83f185f800c77ef0d88a94fb042a7
Arbaz Ansari <abzansari19@gmail.com>
2023-06-18 13:13:51 +0530

    nav menu added
    nav menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,3 +265,14 @@ document.addEventListener("DOMContentLoaded", function(){
   }
   // end if innerWidth
   }); 
+  document.addEventListener("DOMContentLoaded", function(){
+        
+
+    /////// Prevent closing from click inside dropdown
+  document.querySelectorAll('.dropdown-menu').forEach(function(element){
+    element.addEventListener('click', function (e) {
+      e.stopPropagation();
+    });
+  })
+
+});
